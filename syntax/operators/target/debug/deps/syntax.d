01b0f2e46388eb20a806d2ce435679e65649dc91
Christian Gracia <christiangracia.student@careerdevs.com>
2020-10-30 00:56:28 -0400

    added rust component using operators and different types to show syntax

---
+++ syntax/operators/target/debug/deps/syntax.d
@@ -0,0 +1,5 @@
+C:\Users\chris\Documents\rust\syntax\syntax\target\debug\deps\syntax.exe: src\main.rs
+
+C:\Users\chris\Documents\rust\syntax\syntax\target\debug\deps\syntax.d: src\main.rs
+
+src\main.rs:
---
+++ syntax/operators/target/debug/syntax.d
@@ -0,0 +1,1 @@
+C:\Users\chris\Documents\rust\syntax\syntax\target\debug\syntax.exe: C:\Users\chris\Documents\rust\syntax\syntax\src\main.rs
