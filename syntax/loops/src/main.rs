8ef716b7525cf38a77985c86e434abf35ab77c75
Christian Gracia <christiangracia.student@careerdevs.com>
2020-11-02 22:21:43 -0500

    added a few more loop methods and use of a label

--- syntax/loops/src/main.rs
+++ syntax/loops/src/main.rs
@@ -1,15 +1,56 @@
 fn main() {
-   loopMethod(10);
+   loop_method(10);
+   for_loop(10);
+   array_loop();
 }
 
-fn loopMethod(limit: i32){
+fn loop_method(limit: i8){
 
     let mut counter = 0;
     loop {
         counter += 1;
-        println!("{}", counter);
+        println!("{} in loop", counter);
         if counter >= limit {
             return;
         }
+    }
+}
+
+fn for_loop(limit: i8){
+
+    for n in 0..limit {
+          println!("{} in for loop", n);
+    }
+
+}
+
+fn array_loop(){
+
+    let mut v = Vec::new();
+    let c = vec![3,4,5,6,7,87,8];
+
+    v.push(4);
+    v.push(5);
+    v.push(6);
+
+    let mut counter: i8 = 0;
+
+    'outer: for i in 1..10 {
+        for n in &c {
+            counter += 1;
+        
+            if counter == 5 {
+                println!("breaking with label on {}", i);
+                break 'outer;
+            }
+            println!("{} in array", n);
+        }
+    }
+
+    for n in c {
+        if n % 2 == 0 {
+            continue;
+        }
+        println!("{} in other array", n);
     }
 }
