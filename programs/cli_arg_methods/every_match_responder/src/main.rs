aa3451eab522e3cd721e1facae7711999ffe5666
Christian Gracia <christiangracia.student@careerdevs.com>
2020-11-10 22:37:24 -0500

    added a better version of a method that looks for a match and responds using options + match

--- programs/cli_arg_methods/every_match_responder/src/main.rs
+++ programs/cli_arg_methods/every_match_responder/src/main.rs
@@ -1,17 +1,29 @@
 
-// use arguments in run command annd this will say hi to any animal whos letter starts with c
+// use arguments in run command and this will find a match to any arg whos letter starts with c
 //run with cargo run -- dog cat horse lizard
 
 use std::env;
 
 fn main() {
     match_responder(env::args().collect(), 'c');
+    match_responder_better();
 }
 
 fn match_responder(args: Vec<String>, letter: char) -> () {
     for a in args {
         if a.chars().next().unwrap() == letter {
             println!("Match found: {}", a);
+        }
+    }
+}
+
+fn match_responder_better() -> () {
+    for a in env::args() {
+        if let Some(c) = a.chars().next() {
+            match c {
+                'c' | 'C' => println!("Match found in v2: {}", a),
+                _ => {}
+            }
         }
     }
 }
